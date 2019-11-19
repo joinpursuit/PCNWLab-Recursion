20c5b6b9298196a0c2cc2852b0afd4f9baea56c6
Jay Fowler <jcafowler@gmail.com>
2019-11-18 21:12:06 -0500

    Solve getInRange

--- recursionLab2_Solutions.js
+++ recursionLab2_Solutions.js
@@ -3,3 +3,19 @@ const factorial = (n) => {
   return n * factorial(n - 1)
 }
 console.log(factorial(5));
+
+const getInRange = (n1,n2, output = []) => {
+ if (n1 < n2) { 
+   output.push(n1)
+  return output.concat(getInRange(n1 + 1, n2))
+  } else if (n1 > n2){
+    output.push(n2)
+    return output.concat(getInRange(n2 + 1, n1))
+  } else {
+    output.push(n1)
+    return output
+  }
+
+}
+
+console.log(getInRange(10))
